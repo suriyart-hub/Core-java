9a5e03bf93ae1910df4574f6659e90970e4a8dde
suriyart-hub <56015448+suriyart-hub@users.noreply.github.com>
2021-09-29 11:41:30 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,21 @@
+package core_java;
+
+public class datatypes {
+
+	public static void main(String[] args) {
+		System.out.println("hello suriya");
+		// #primitive
+		byte c= 103;
+		short d= 16104;
+		int e= 809834;
+		long  f = 80983434;
+		char a= 'c';
+		float g = 1.9f;
+		double h = 23.45556666666666;
+		// #non primitive
+		String b= "A CV should be short and to the point. Include: Personal information including surname; first names; date and country of birth; identity number; male/female; married/single; state of your health; driver's licence; address and contact numbers. Educational history";
+		System.out.println(a+"\n"+b+"\n"+c+"\n"+d+"\n"+e+"\n"+f+"\n"+g+"\n"+h);
+
+	}
+
+}
