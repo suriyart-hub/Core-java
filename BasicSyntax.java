fe68a2b4255c7afafe267799fe54bac60fadf5f4
suriyart-hub <56015448+suriyart-hub@users.noreply.github.com>
2021-09-29 11:40:41 +0530

    Add files via upload

---
+++ BasicSyntax.java
@@ -0,0 +1,17 @@
+package core_java;
+
+public class BasicSyntax {
+
+	public static void main(String[] args) {
+		//public=Access modifier
+		//void =nothing return
+		//string=class
+		//args=argument
+		
+		
+		System.out.println("Welcome Home");
+		//sout print statement
+
+	}
+
+}
